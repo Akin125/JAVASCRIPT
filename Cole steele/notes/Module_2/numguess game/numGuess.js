eacb8353eeb4b0a634772119e797483b50e64c3a
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-01-27 00:48:56 +0100

    done with guessing game

--- Cole steele/notes/Module_2/numguess game/numGuess.js
+++ Cole steele/notes/Module_2/numguess game/numGuess.js
@@ -1,1 +1,24 @@
- alert("boss");
+// create a secret number
+// collect users guess
+// check if correct
+// check if wrong
+// check if higer
+// check if lower 
+
+var secretNum = 6;
+
+var stringGuess = prompt("Enter your guess:");
+
+numGuess = Number(stringGuess);
+
+if(secretNum === numGuess){
+    alert("Your guess is corect");
+}
+else if (secretNum > numGuess){
+    alert("Guess is low, try again");
+}
+
+else if (secretNum < numGuess){
+    alert("Guess is low, try again");
+}
+
